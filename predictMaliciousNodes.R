b58dd7aaae26073ff877e2a0846f88ab4d4794fc
jakobgerstenlauer <jakob.gerstenlauer@est.fib.upc.edu>
2017-05-09 14:14:20 +0200

    new global switch runStats: should statistical models be run or not?

--- predictMaliciousNodes.R
+++ predictMaliciousNodes.R
@@ -52,6 +52,9 @@ logging<-function(logMessage, outputfile){
   setwd(oldPath)
 }
 
+#TODO Set TRUE to run statistical analyses!
+runStats <- FALSE
+
 #TODO Increase after first test run!
 num.trees <- 300
 
@@ -94,11 +97,11 @@ for(year in seq(2008,2017)){
   d<-read.table(fileName, header=FALSE, sep=";",quote="",  
                 stringsAsFactors=FALSE,comment.char="",                   
                 colClasses=col.type)
+  names(d)<-header
   
   num.obs[index]<-dim(d)[1]
   num.ips[index]<-length(unique(d$IP))
-  names(d)<-header
-
+  
   #set correct data type for inputs
   d$BadExit   <- as.factor(d$BadExit)
   d$Port      <- as.factor(d$Port)
@@ -122,6 +125,11 @@ for(year in seq(2008,2017)){
   logging(msg, outputfile=logFile)
   num.ips.flag.bad[index]<-length(bad.ips)
   
+  d$isSybill  <- as.factor(d$IP %in% IPs.Sybill)
+  index.ip<-which(names(d)=="IP")
+  num.sybills[index] <- length(unique(d[d$isSybill,index.ip])) 
+  gc()
+  
   outputFileName<-glue("IPs_BadExit_",year,".txt")
   setwd(dataDir)
   write.table(bad.ips, file=outputFileName,append=FALSE,col.names=FALSE)
@@ -134,131 +142,128 @@ for(year in seq(2008,2017)){
   print(histogram(~log(table(d$IP))))
   dev.off()
   
-  #Automatically remove inputs without variance:
-  calculate.variance<-function(x){
-    var(as.numeric(x), na.rm = TRUE)
-  }
-  variances<-as.numeric(lapply(d[,8:18], calculate.variance))
-  positive.variances <-ifelse(is.na(variances),FALSE,variances>0)
-  indices<-c(rep(TRUE,7), positive.variances) 
-  d<-d[,indices]
-  
-  d$isSybill  <- as.factor(d$IP %in% IPs.Sybill)
-  index.ip<-which(names(d)=="IP")
-  num.sybills[index] <- length(unique(d[d$isSybill,index.ip])) 
-  gc()
-  
-  msg<-glue("Logistic regression models: ")
-  logging(msg, outputfile=logFile)
-  
-  #logistic regression of BadExit
-  if("BadExit" %in% names(d)){
-    predictors<-names(d)[-c(1:4)]
-    predictors<-predictors[predictors!="BadExit"]
-    #There is a problem with this predictor (with the contrasts)
-    predictors<-predictors[predictors!="NoEdConsensus"]
-    
-    #take a subsample of the observations which don't have the "BadExit" flag:
-    indicesBad<-which(d$BadExit==1)
-    indicesGood<-which(d$BadExit==0)
-    
-    #I want to have a balanced sample!
-    #Therefore I retain all observations with the flag BadExit,
-    #and I randomly sample the same number of observation from the subset 
-    #of 
-    N<-length(indicesBad)
-    if(N > 20000){
-      N <- 20000;
-      indicesBad<-sample(indicesBad, size = N, replace = FALSE);
-    }
-    samplesGood <- sample(indicesGood, size = N, replace = FALSE);
-    sampleIndices <- sort(c(samplesGood, indicesBad));
-    
-    bestPredictor <- predictors[1]
-    bestAIC<- AIC(glm(BadExit ~ 1, family= binomial(), data=d[sampleIndices,]))
-       
-    for(predictor in predictors){
-      eval(parse(text=glue(
-        "m1.glm <- glm(BadExit ~ ",predictor," , family= binomial(), data=d[sampleIndices,])"
-      )))
-      aic<-AIC(m1.glm)
-      msg<-glue(predictor," : AIC:",aic)
-      logging(msg, outputfile=logFile)
-      if(aic < bestAIC){
-        bestAIC<-aic;
-        bestPredictor <- predictor;
+  if(runStats){
+    #logistic regression of BadExit
+    if("BadExit" %in% names(d)){
+      #Automatically remove inputs without variance:
+      calculate.variance<-function(x){
+        var(as.numeric(x), na.rm = TRUE)
       }
-    }
-    FirstPredictor<-bestPredictor
-    predictors<-predictors[predictors!=FirstPredictor]
-    
-    msg<-glue("Best predictor in round 1 is: ", FirstPredictor)
-    logging(msg, outputfile=logFile)
-    
-    #Round 2
-    for(predictor in predictors){
-      eval(parse(text=glue(
-        "m1.glm <- glm(BadExit ~ ",FirstPredictor," + ",predictor," , family= binomial(), data=d[sampleIndices,])"
-      )))
-      aic<-AIC(m1.glm)
-      msg<-glue(predictor," : AIC:",aic)
+      variances<-as.numeric(lapply(d[,8:18], calculate.variance))
+      positive.variances <-ifelse(is.na(variances),FALSE,variances>0)
+      indices<-c(rep(TRUE,7), positive.variances) 
+      d<-d[,indices]
+      
+      msg<-glue("Logistic regression models: ")
       logging(msg, outputfile=logFile)
-      if(aic < bestAIC){
-        bestAIC<-aic;
-        bestPredictor <- predictor;
+      
+      predictors<-names(d)[-c(1:4)]
+      predictors<-predictors[predictors!="BadExit"]
+      #There is a problem with this predictor (with the contrasts)
+      predictors<-predictors[predictors!="NoEdConsensus"]
+      
+      #take a subsample of the observations which don't have the "BadExit" flag:
+      indicesBad<-which(d$BadExit==1)
+      indicesGood<-which(d$BadExit==0)
+      
+      #I want to have a balanced sample!
+      #Therefore I retain all observations with the flag BadExit,
+      #and I randomly sample the same number of observation from the subset 
+      #of 
+      N<-length(indicesBad)
+      if(N > 20000){
+        N <- 20000;
+        indicesBad<-sample(indicesBad, size = N, replace = FALSE);
       }
-    }
-    
-    SecondPredictor<-bestPredictor
-    predictors<-predictors[predictors!=SecondPredictor]
-    
-    msg<-glue("Best predictor in round 2 is: ", SecondPredictor)
-    logging(msg, outputfile=logFile)
-    
-    #Round 3
-    for(predictor in predictors){
-      eval(parse(text=glue(
-        "m1.glm <- glm(BadExit ~ ",FirstPredictor," + ",SecondPredictor," +",predictor," , family= binomial(), data=d[sampleIndices,])"
-      )))
-      aic<-AIC(m1.glm)
-      msg<-glue(predictor," : AIC:",aic)
+      samplesGood <- sample(indicesGood, size = N, replace = FALSE);
+      sampleIndices <- sort(c(samplesGood, indicesBad));
+      
+      bestPredictor <- predictors[1]
+      bestAIC<- AIC(glm(BadExit ~ 1, family= binomial(), data=d[sampleIndices,]))
+      
+      for(predictor in predictors){
+        eval(parse(text=glue(
+          "m1.glm <- glm(BadExit ~ ",predictor," , family= binomial(), data=d[sampleIndices,])"
+        )))
+        aic<-AIC(m1.glm)
+        msg<-glue(predictor," : AIC:",aic)
+        logging(msg, outputfile=logFile)
+        if(aic < bestAIC){
+          bestAIC<-aic;
+          bestPredictor <- predictor;
+        }
+      }
+      FirstPredictor<-bestPredictor
+      predictors<-predictors[predictors!=FirstPredictor]
+      
+      msg<-glue("Best predictor in round 1 is: ", FirstPredictor)
       logging(msg, outputfile=logFile)
-      if(aic < bestAIC){
-        bestAIC<-aic;
-        bestPredictor <- predictor;
+      
+      #Round 2
+      for(predictor in predictors){
+        eval(parse(text=glue(
+          "m1.glm <- glm(BadExit ~ ",FirstPredictor," + ",predictor," , family= binomial(), data=d[sampleIndices,])"
+        )))
+        aic<-AIC(m1.glm)
+        msg<-glue(predictor," : AIC:",aic)
+        logging(msg, outputfile=logFile)
+        if(aic < bestAIC){
+          bestAIC<-aic;
+          bestPredictor <- predictor;
+        }
       }
-    }
-    
-    ThirdPredictor<-bestPredictor
-    predictors<-predictors[predictors!=ThirdPredictor]
-    
-    msg<-glue("Best predictor in round 3 is: ", ThirdPredictor)
-    logging(msg, outputfile=logFile)
-    
-    #Round 4
-    for(predictor in predictors){
-      eval(parse(text=glue(
-        "m1.glm <- glm(BadExit ~ ",FirstPredictor," + ",SecondPredictor," + ",ThirdPredictor," +",predictor," , family= binomial(), data=d[sampleIndices,])"
-      )))
-      aic<-AIC(m1.glm)
-      msg<-glue(predictor," : AIC:",aic)
+      
+      SecondPredictor<-bestPredictor
+      predictors<-predictors[predictors!=SecondPredictor]
+      
+      msg<-glue("Best predictor in round 2 is: ", SecondPredictor)
       logging(msg, outputfile=logFile)
-      if(aic < bestAIC){
-        bestAIC<-aic;
-        bestPredictor <- predictor;
+      
+      #Round 3
+      for(predictor in predictors){
+        eval(parse(text=glue(
+          "m1.glm <- glm(BadExit ~ ",FirstPredictor," + ",SecondPredictor," +",predictor," , family= binomial(), data=d[sampleIndices,])"
+        )))
+        aic<-AIC(m1.glm)
+        msg<-glue(predictor," : AIC:",aic)
+        logging(msg, outputfile=logFile)
+        if(aic < bestAIC){
+          bestAIC<-aic;
+          bestPredictor <- predictor;
+        }
       }
+      
+      ThirdPredictor<-bestPredictor
+      predictors<-predictors[predictors!=ThirdPredictor]
+      
+      msg<-glue("Best predictor in round 3 is: ", ThirdPredictor)
+      logging(msg, outputfile=logFile)
+      
+      #Round 4
+      for(predictor in predictors){
+        eval(parse(text=glue(
+          "m1.glm <- glm(BadExit ~ ",FirstPredictor," + ",SecondPredictor," + ",ThirdPredictor," +",predictor," , family= binomial(), data=d[sampleIndices,])"
+        )))
+        aic<-AIC(m1.glm)
+        msg<-glue(predictor," : AIC:",aic)
+        logging(msg, outputfile=logFile)
+        if(aic < bestAIC){
+          bestAIC<-aic;
+          bestPredictor <- predictor;
+        }
+      }
+      
+      #Write final model with four predictors to text output file:
+      FourthPredictor<-bestPredictor
+      eval(parse(text=glue(
+        "m1.glm <- glm(BadExit ~ ",FirstPredictor," + ",SecondPredictor," + ",ThirdPredictor," +",FourthPredictor," , family= binomial(), data=d[sampleIndices,])"
+      )))
+      fileName<-glue("ModelSummary_",year,".txt")
+      setwd(dataDir)
+      sink(fileName)
+      summary(m1.glm)
+      sink()  
     }
-    
-    #Write final model with four predictors to text output file:
-    FourthPredictor<-bestPredictor
-    eval(parse(text=glue(
-      "m1.glm <- glm(BadExit ~ ",FirstPredictor," + ",SecondPredictor," + ",ThirdPredictor," +",FourthPredictor," , family= binomial(), data=d[sampleIndices,])"
-    )))
-    fileName<-glue("ModelSummary_",year,".txt")
-    setwd(dataDir)
-    sink(fileName)
-    summary(m1.glm)
-    sink()  
   }
   
   #perform a join with the data set containing the information about change of fingerprints 
@@ -294,89 +299,45 @@ for(year in seq(2008,2017)){
   print(histogram(~d$count))
   dev.off()
   
-  require("gbm")
-  #check the number of factors of gbm, only 1024 are allowed!
-  d$Port <- factor(d$Port)
-  numOfFactors<-length(levels(d$Port))
-  if(numOfFactors>1024){
-    stop("There are more than 1024 factor levels / ports! Thus the influence of the ports has to be ignored.")
-  }
-
-  #fit the model only for subset of data with data about fingerprint changes
-  tryCatch({
-  m1.gbm <- gbm (count ~ . ,
-                 distribution="poisson",
-                 verbose=FALSE,
-                 interaction.depth=3,
-                 shrinkage=0.001,
-                 n.trees = num.trees,
-                 #ignore ports, because there are to many levels!
-                 data=d[,names(d)[-c(1:4)]])
-  ri<-summary(m1.gbm, plotit=FALSE)
-  outputFileName<-glue("VariableImportanceBoostedRegressionTrees_Fingerprint_Count",year,".txt")
-  setwd(dataDir)
-  write.table(ri, file=outputFileName,append=FALSE,col.names=FALSE)
-  
-  logging("The most important variables (var importance > 3%)for the prediction of suspicious IPs based on fingerprint changes are:",outputfile=logFile)
-  logging(as.character(ri[ri$rel.inf>3 , 1]), outputfile=logFile) 
-  
-  d$predictions<-predict(m1.gbm, d, n.trees=num.trees, type="response")
-  rm(m2.gbm)
-  }, error = function(e) {
-    rm(m2.gbm);
-  }, finally = {
-    rm(m2.gbm);
-  })
-  
-  #number of observations
-  N<-dim(d)[1]
- 
-  port.observations<-with(d,tapply(predictions, Port, length))
-  #Let's restrict the analysis to ports with at least 5 observations:
-  ports<-(names(port.observations[port.observations>4]))
-  ports<-ports[!is.na(ports)]
-  dx<-subset(d, Port %in% ports)
-  suspicious.ports<-sort(with(dx, tapply(predictions, Port, mean)))
-  rm(dx)
-  n<-length(suspicious.ports)
-  most.suspicious.ports<-suspicious.ports[seq((n-20),n)]
-  #9080     9002    49000       21     9005    20017 
-  #1.338371 1.418677 1.508181 1.510889 1.516973 1.529711 
-  
-  logging("The 10 most suspicious ports based on fingerprint changes (model results): ",outputfile=logFile) 
-  logging(names(most.suspicious.ports), outputfile=logFile) 
-  rm(m1.gbm)
-  
-  #************************************************************************************************
-  #if there is no variance in BadExit it is removed!
-  if(exists("d$BadExit")){
- 
+  if(runStats){
+    require("gbm")
+    #check the number of factors of gbm, only 1024 are allowed!
+    d$Port <- factor(d$Port)
+    numOfFactors<-length(levels(d$Port))
+    if(numOfFactors>1024){
+      stop("There are more than 1024 factor levels / ports! Thus the influence of the ports has to be ignored.")
+    }
+    
+    #fit the model only for subset of data with data about fingerprint changes
     tryCatch({
-    m2.gbm <- gbm (BadExit ~ . ,
-                   distribution="bernoulli",
-                   verbose=FALSE,
-                   interaction.depth=3,#6
-                   shrinkage=0.001,#0.001
-                   n.trees = num.trees,#3000
-                   data=d[1,names(d)[-c(1:4)]])
-  
-    ri<-summary(m2.gbm, plotit=FALSE)
-    outputFileName<-glue("VariableImportanceBoostedRegressionTrees_BadExit",year,".txt")
-    setwd(dataDir)
-    write.table(ri, file=outputFileName,append=FALSE,col.names=FALSE)
-    
-    logging("The most important variables (var importance > 3%)for the prediction of BadExit IPs:",outputfile=logFile)
-    logging(as.character(ri[ri$rel.inf>3 , 1]), outputfile=logFile) 
-    
-    d$predictions2<-predict(m2.gbm, d, n.trees=num.trees, type="response")
-    rm(m2.gbm)
+      m1.gbm <- gbm (count ~ . ,
+                     distribution="poisson",
+                     verbose=FALSE,
+                     interaction.depth=3,
+                     shrinkage=0.001,
+                     n.trees = num.trees,
+                     #ignore ports, because there are to many levels!
+                     data=d[,names(d)[-c(1:4)]])
+      ri<-summary(m1.gbm, plotit=FALSE)
+      outputFileName<-glue("VariableImportanceBoostedRegressionTrees_Fingerprint_Count",year,".txt")
+      setwd(dataDir)
+      write.table(ri, file=outputFileName,append=FALSE,col.names=FALSE)
+      
+      logging("The most important variables (var importance > 3%)for the prediction of suspicious IPs based on fingerprint changes are:",outputfile=logFile)
+      logging(as.character(ri[ri$rel.inf>3 , 1]), outputfile=logFile) 
+      
+      d$predictions<-predict(m1.gbm, d, n.trees=num.trees, type="response")
+      rm(m2.gbm)
     }, error = function(e) {
-        rm(m2.gbm);
+      rm(m2.gbm);
     }, finally = {
-        rm(m2.gbm);
+      rm(m2.gbm);
     })
     
-    port.observations<-with(d,tapply(predictions2, Port, length))
+    #number of observations
+    N<-dim(d)[1]
+    
+    port.observations<-with(d,tapply(predictions, Port, length))
     #Let's restrict the analysis to ports with at least 5 observations:
     ports<-(names(port.observations[port.observations>4]))
     ports<-ports[!is.na(ports)]
@@ -385,10 +346,56 @@ for(year in seq(2008,2017)){
     rm(dx)
     n<-length(suspicious.ports)
     most.suspicious.ports<-suspicious.ports[seq((n-20),n)]
+    #9080     9002    49000       21     9005    20017 
+    #1.338371 1.418677 1.508181 1.510889 1.516973 1.529711 
     
-    logging("The 10 most suspicious ports based on BadExit flags (model results): ",outputfile=logFile) 
+    logging("The 10 most suspicious ports based on fingerprint changes (model results): ",outputfile=logFile) 
     logging(names(most.suspicious.ports), outputfile=logFile) 
+    rm(m1.gbm)
+    
+    #************************************************************************************************
+    #if there is no variance in BadExit it is removed!
+    if(exists("d$BadExit")){
+      
+      tryCatch({
+        m2.gbm <- gbm (BadExit ~ . ,
+                       distribution="bernoulli",
+                       verbose=FALSE,
+                       interaction.depth=3,#6
+                       shrinkage=0.001,#0.001
+                       n.trees = num.trees,#3000
+                       data=d[1,names(d)[-c(1:4)]])
+        
+        ri<-summary(m2.gbm, plotit=FALSE)
+        outputFileName<-glue("VariableImportanceBoostedRegressionTrees_BadExit",year,".txt")
+        setwd(dataDir)
+        write.table(ri, file=outputFileName,append=FALSE,col.names=FALSE)
+        
+        logging("The most important variables (var importance > 3%)for the prediction of BadExit IPs:",outputfile=logFile)
+        logging(as.character(ri[ri$rel.inf>3 , 1]), outputfile=logFile) 
+        
+        d$predictions2<-predict(m2.gbm, d, n.trees=num.trees, type="response")
+        rm(m2.gbm)
+      }, error = function(e) {
+        rm(m2.gbm);
+      }, finally = {
+        rm(m2.gbm);
+      })
+      
+      port.observations<-with(d,tapply(predictions2, Port, length))
+      #Let's restrict the analysis to ports with at least 5 observations:
+      ports<-(names(port.observations[port.observations>4]))
+      ports<-ports[!is.na(ports)]
+      dx<-subset(d, Port %in% ports)
+      suspicious.ports<-sort(with(dx, tapply(predictions, Port, mean)))
+      rm(dx)
+      n<-length(suspicious.ports)
+      most.suspicious.ports<-suspicious.ports[seq((n-20),n)]
+      
+      logging("The 10 most suspicious ports based on BadExit flags (model results): ",outputfile=logFile) 
+      logging(names(most.suspicious.ports), outputfile=logFile) 
     }
+  }
     rm(d);gc();
     index<-index+1;
   }
