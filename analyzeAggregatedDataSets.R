479c3311041f442bd508cf6067f59fe021bc39f8
Francisco Perez <pacogppl@hotmail.com>
2017-05-12 20:44:59 +0200

    Commit

--- analyzeAggregatedDataSets.R
+++ analyzeAggregatedDataSets.R
@@ -163,36 +163,6 @@ for(year in seq(2009,2017)){
   }
 }
 
-setwd(dataDir)
 
-# export results to table
-require(xtable)
-check.significance<-function(x){
-  return(ifelse(x!="",ifelse(x<0.05,"+","-"),"N/A"))
-}
-
-rownames<-c("Year", "Bandwidth", "Fast","HSDir","V2Dir","Num Observations","Null Deviance","Residual Deviance","R2")
-
-d.out<-NULL
-d.out<-cbind(d.out,rownames)
-for(year in seq(2009,2017)){
-  fileName<-glue("GLM_FullModel_Summary_Aggregated_",year,".txt")
-  if(exists(fileName)){
-    d<-read.table(fileName, header=FALSE, sep=" ",stringsAsFactors=FALSE,comment.char="")
-    names(d)<-header
-    row.out<-c(year,check.significance(d$Bandwidth),
-               check.significance(d$Fast),
-               check.significance(d$HSDir),
-               check.significance(d$V2Dir),
-               check.significance(d$NumObservations),
-               d$NullDeviance,
-               d$ResidualDeviance,d$R2)
-    cbind(d.out,row.out)
-  }else{
-    cbind(d.out,row.out)
-  }
-}
-
-xtable(d.out)
 
   
